e3fdfb047304c10120f4065c533dee9e9352b8d4
bu1 <63654297+fifakillmen@users.noreply.github.com>
2024-05-17 10:04:49 +0700

    create project

---
+++ SWD392-Back-end/SWD392-Back-end/src/main/java/com/v1/SWD392Backend/Swd392BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.v1.SWD392Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swd392BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swd392BackEndApplication.class, args);
+	}
+
+}
---
+++ SWD392-Back-end/SWD392-Back-end/src/test/java/com/v1/SWD392Backend/Swd392BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.SWD392Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swd392BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
